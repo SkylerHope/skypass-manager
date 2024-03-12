2a398ea91723432f47ad9646e4b47fdfd062708e
SkylerHope <elpidakouro13@gmail.com>
2024-03-12 13:39:09 +0200

    fixing starting issues

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+  const win = new BrowserWindow({
       width: 800,
       height: 600
-    })
+  })
   
-    win.loadFile('index.html')
+  win.loadFile('index.html')
 }
+
+app.whenReady().then(createWindow)
