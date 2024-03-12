9638da595f8798b294da1899f05c29a739a863c3
SkylerHope <elpidakouro13@gmail.com>
2024-03-12 15:03:59 +0200

    addressing security issues

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
       width: 800,
-      height: 600
-  })
+      height: 600,
+      icon: path.join(__dirname, 'assets', 'img', 'favicon.ico'),
+      webPreferences: {
+        contentSecurityPolicy: "script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;"
+      }
+  });
   
-  win.loadFile('index.html')
-}
+  win.loadFile('index.html');
+};
 
 app.whenReady().then(createWindow)
