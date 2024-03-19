b7492a70348296fe1273dcc0f8f57fa22c999288
SkylerHope <elpidakouro13@gmail.com>
2024-03-19 13:17:33 +0200

    error hotfix

---
+++ check.js
@@ -0,0 +1,24 @@
+const { ipcRenderer } = require('electron');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('login-pin-form');
+    const loginPin = document.getElementById('login-pin');
+    //const loginError = document.getElementById('login-error-message');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const pinInput = loginPin.value;
+
+        ipcRenderer.send('verify-pin', pinInput);
+    });
+});
+
+ipcRenderer.on('pin-verify-result', (event, isPinCorrect) => {
+    if(isPinCorrect) {
+        console.log('PIN is correct!');
+        window.location.href('index.html');
+    } else {
+        console.log('PIN is false!');
+    }
+});
