911e7f64b78ce585057bf008c73ff3a69f4aabe0
SkylerHope <elpidakouro13@gmail.com>
2024-03-19 12:07:58 +0200

    making progress in decryption

--- check.js
+++ check.js
@@ -1,2 +1,15 @@
 const crypto = require('crypto');
 let { algorithm, key, iv } = require('./main');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('login-pin-form');
+    const loginError = document.getElementById('login-error-message');
+    const loginPin = document.getElementById('login-pin');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const decipher = crypto.createDecipheriv(algorithm, key, iv);
+        let decryptedPin = decipher.update()
+    })
+})
