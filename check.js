ae5f5bda61b5421af229062cd6f95c1836faf9e0
SkylerHope <elpidakouro13@gmail.com>
2024-03-22 10:51:17 +0200

    fixed iv redeclare block-scoped var error

--- check.js
+++ check.js
@@ -8,9 +8,9 @@ let encryptedPin = fs.readFileSync('config.enc', 'utf8');
 const algorithm = 'aes-256-cbc';
 
 let iv2 = fs.readFileSync('config.enc', 'utf8').substring(0, 16);
-let key = fs.readFileSync('config.enc', 'utf8').substring(0, 32);
+let key2 = fs.readFileSync('config.enc', 'utf8').substring(0, 32);
 
-const decipher = crypto.createDecipheriv(algorithm, Buffer.from (key, 'hex'), Buffer.from(iv2, 'hex'));
+const decipher = crypto.createDecipheriv(algorithm, Buffer.from (key2, 'hex'), Buffer.from(iv2, 'hex'));
 let decryptedPin = decipher.update(encryptedPin, 'hex', 'utf8'); 
 decryptedPin += decipher.final('utf8');
 
